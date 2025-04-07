9409be3bb9f2a7f253ded451ff5385c6077aad73
escuelaguirre4@gmail.com <escuelaguirre4@gmail.com>
2025-04-07 11:56:59 -0300

    creamos una pagina en las rutas con la herramienta url_for

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
 from flask import Flask
-
+from flask import url_for
 app = Flask(__name__)
+
+@app.route('/')
+def main():
+    url_hola =  url_for("hello")
+    url_dado = url_for ("dados", caras=8)
+    url_logo= url_for("static", filename="logo.jpg")
+    return f"""
+    <a href="{url_hola}">saludar</a>
+    <br>
+    <a href="{url_dado}">tirar</a>
+    <br>
+    <a href="{url_logo}">logo</a>
+    
+    
+    """
 
 
 @app.route('/saludar')
@@ -11,12 +26,10 @@ def hello():
 def gsdh():
     return 'dtyjj'
 
-@app.route('/tirar/<int:dado>')
+@app.route('/tirar/<int:caras>')
 def dados(caras):
     from random import randint
     ca = randint(1, caras)
-
-
     return f'<p>tire un dado de {caras}, salio {ca}<p>'
 
 
@@ -28,3 +41,6 @@ def suma(n1,n2):
 @app.route('/nombre/nombre/<string:nombre>')
 def nombre():
     return f'<p>hola {nombre}<p>'
+
+    
+    
